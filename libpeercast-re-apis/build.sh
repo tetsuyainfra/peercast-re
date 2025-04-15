0e613c201a3070cf3f32327f9ddfccdf1f1c2dae
tetsuyainfra <tetsuyainfra@gmail.com>
2025-04-16 02:38:56 +0900

    fix: shellscript export uid/gid

--- libpeercast-re-apis/build.sh
+++ libpeercast-re-apis/build.sh
@@ -2,11 +2,10 @@
 
 set -ex
 SCRIPT_ROOT=$(cd $(dirname $0);pwd)
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
 
 pushd $SCRIPT_ROOT
-    id
     docker compose -f docker-compose.build-api.yml  --profile api up --remove-orphans
     ls -l gen
 popd
