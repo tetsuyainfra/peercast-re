6da5d2789ec82e6a5f90a63b41e386db91edef79
tetsuyainfra <tetsuyainfra@gmail.com>
2025-04-16 02:26:58 +0900

    fix: use native user/group id in container

--- libpeercast-re-apis/build.sh
+++ libpeercast-re-apis/build.sh
@@ -2,8 +2,10 @@
 
 set -ex
 SCRIPT_ROOT=$(cd $(dirname $0);pwd)
+UID=$(id -u)
+GID=$(id -u)
 
 pushd $SCRIPT_ROOT
-    docker compose -f docker-compose.build-api.yml  --profile api up --remove-orphans
+    UID=${UID} GID=${GID} docker compose -f docker-compose.build-api.yml  --profile api up --remove-orphans
 popd
 
