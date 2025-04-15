7b4ee86f0362fcbf3411ae1eea5f9b6450067ae1
tetsuyainfra <tetsuyainfra@gmail.com>
2025-04-16 02:30:49 +0900

    fix: shellscript variable

--- libpeercast-re-apis/build.sh
+++ libpeercast-re-apis/build.sh
@@ -2,8 +2,6 @@
 
 set -ex
 SCRIPT_ROOT=$(cd $(dirname $0);pwd)
-UID=$(id -u)
-GID=$(id -u)
 
 pushd $SCRIPT_ROOT
     UID=${UID} GID=${GID} docker compose -f docker-compose.build-api.yml  --profile api up --remove-orphans
