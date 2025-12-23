e548326cacb1e302627b24d5a8dfcf8795db88bd
tetsuyainfra <tetsuyainfra@gmail.com>
2025-12-24 00:41:34 +0900

    tracingモジュールのインポートを整理し、スパン関連のアイテムを個別に公開

--- peercast-re/src/prelude.rs
+++ peercast-re/src/prelude.rs
@@ -1,1 +1,3 @@
-pub use tracing::{Level, debug, error, info, instrument, span, trace, warn};
+pub use tracing::{Level, Span, instrument, span};
+pub use tracing::{debug, error, info, trace, warn};
+pub use tracing::{debug_span, error_span, info_span, trace_span, warn_span};
