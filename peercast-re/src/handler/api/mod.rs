bac3351dae5dc918933179609d7d2619b66c6763
tetsuyainfra <tetsuyainfra@gmail.com>
2026-01-23 18:45:37 +0900

    /apiのエンドポイントで動作しているAPIを出力するようにした

--- peercast-re/src/handler/api/mod.rs
+++ peercast-re/src/handler/api/mod.rs
@@ -1,7 +1,9 @@
+use axum::{Router, routing};
+
 // src/handler/api/mod.rs
 // API handler
 //
-use crate::{AppState, SWAGGER_PATH};
+use crate::AppState;
 
 pub mod v1;
 pub mod v2;
@@ -9,6 +11,12 @@ pub mod v2;
 pub fn build_router() -> axum::Router<AppState> {
     axum::Router::new()
         //
+        .route("/", routing::get(versions))
         .nest("/v1", v1::router())
         .nest("/v2", v2::router())
+}
+
+async fn versions() -> impl axum::response::IntoResponse {
+    let versions = vec!["v1"];
+    axum::Json(versions)
 }
