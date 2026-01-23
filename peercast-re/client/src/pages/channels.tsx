c798c327624697e37521b1a4974334790d95d5cf
tetsuyainfra <tetsuyainfra@gmail.com>
2026-01-23 18:44:59 +0900

    temp: とりあえずapiの動作を確認

--- peercast-re/client/src/pages/channels.tsx
+++ peercast-re/client/src/pages/channels.tsx
@@ -2,12 +2,29 @@ import React, { useEffect, useState } from "react"
 import { env } from "process"
 import MainLayout from "@/layouts/mainLayout"
 // import { RespChannel } from "@re-api"
+import { client } from "@/api/client.gen"
+import { createClient } from "@/api/client"
+import { listChannels } from "@/api"
+
+// configure internal service client
+client.setConfig({
+  // set default base url for requests
+  baseUrl: "http://localhost:5173",
+  // set default headers for requests
+  headers: {
+    Authorization: "Bearer <token_from_service_client>",
+  },
+})
 
 export default function Channels() {
   const urlInputId = React.useId()
-  let [channels, channelsSet] = useState<any []>([])
+  let [channels, channelsSet] = useState<any[]>([])
   useEffect(() => {
-    ;
+    (async () => {
+      const { data, error } = await listChannels();
+      console.info("listChannels", { data, error });
+      channelsSet(data as any || []);
+    })()
     // (async () => {
     //   let api = new ChannelApi(api_config())
     //   await api.channelsGet().then(
@@ -31,23 +48,27 @@ export default function Channels() {
     let id = url.pathname.split("/").at(-1) || ""
     let host = url.searchParams.get("tip") || ""
 
-  // let api = new ChannelApi(api_config())
-  //   api
-  //     .createRelayChannel({
-  //       reqCreateRelayChannel: {
-  //         id: id,
-  //         host: host,
-  //       },
-  //     })
-  //     .then((channel) => {
-  //       console.log("createRelayChannel", channel)
-  //       window.location.reload()
-  //     })
+    // let api = new ChannelApi(api_config())
+    //   api
+    //     .createRelayChannel({
+    //       reqCreateRelayChannel: {
+    //         id: id,
+    //         host: host,
+    //       },
+    //     })
+    //     .then((channel) => {
+    //       console.log("createRelayChannel", channel)
+    //       window.location.reload()
+    //     })
   }
 
-  return (
-    <MainLayout pageTitle="Channels">
-      Channels
+  return <MainLayout pageTitle="Channels">
+    Channels
+    {channels.map((channel) => (
+      <div key={channel.id}>
+        <h2>{channel.name} ({channel.id})</h2>
+        <p>{channel.description}</p>
+      </div>
+    ))}
     </MainLayout>
-  )
 }
