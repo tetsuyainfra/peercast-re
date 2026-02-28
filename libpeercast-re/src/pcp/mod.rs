8c794f5503fb1ec1f614b484e7dd1786a3a654b0
tetsuyainfra <tetsuyainfra@gmail.com>
2026-03-01 04:23:18 +0900

    lib: Atom2に新しい型Atom2KindとAtomViewを追加

--- libpeercast-re/src/pcp/mod.rs
+++ libpeercast-re/src/pcp/mod.rs
@@ -27,7 +27,7 @@ mod tracker_channel;
 mod util;
 
 pub use atom::{decode, encode, read_atom, Atom, ChildAtom, ParentAtom};
-pub use atom2::{atom_mut::AtomMut, codec::AtomCodec, Atom2};
+pub use atom2::{atom_mut::AtomMut, codec::AtomCodec, Atom2, Atom2Kind, AtomView};
 pub use channel::*;
 pub use channel2::{ValidChannelInfo, ValidTrackInfo};
 pub use connection::PcpConnectionFactory;
