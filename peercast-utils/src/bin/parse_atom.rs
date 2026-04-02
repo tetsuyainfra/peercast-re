c7f62996881a891d78ca8a822e535c4df43db8be
tetsuyainfra <tetsuyainfra@gmail.com>
2026-04-02 13:46:12 +0900

    peercast-utils: libpeercast-renewを使用するように変更

--- peercast-utils/src/bin/parse_atom.rs
+++ peercast-utils/src/bin/parse_atom.rs
@@ -1,16 +1,20 @@
-use bytes::BytesMut;
-use libpeercast_re::pcp::read_atom;
+use futures_util::{StreamExt, TryStreamExt};
+use libpeercast_renew::atom::{AtomView, codec::AtomCodec};
 use tokio::fs::File;
+use tokio_util::codec::Framed;
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
     let filename = args.get(1).expect("need FILENAME");
 
-    let mut file = File::open(filename).await.expect("file not found");
+    let file = File::open(filename).await.expect("file not found");
 
-    let mut buf = BytesMut::new();
-    let atom = read_atom(&mut file, &mut buf).await;
-
-    println!("{:#?}", atom);
+    let framed = Framed::new(file, AtomCodec::new());
+    let _ = framed
+        .try_for_each(|atom| async move {
+            println!("Atom: kind={:?}, length={}, payload={:?}", atom.kind(), atom.length(), atom.payload());
+            Ok(())
+        })
+        .await;
 }
--- peercast-utils/src/bin/ping.rs
+++ peercast-utils/src/bin/ping.rs
@@ -5,10 +5,7 @@
 //  TODO: Tracker/Relayに対してポートチェックしてもらう通信をおこなう機能を実装する
 //  MEMO: portcheckしてもらうにはHttpでChannelIdを通知する必要がある
 use clap::Parser;
-use futures_util::StreamExt;
-use libpeercast_re::pcp::{AtomCodec, GnuId, procedure::handshake::OutgoingPcpHandshake};
-use tokio::net::TcpListener;
-use tokio_util::codec::Framed;
+use libpeercast_renew::GnuId;
 
 #[derive(Parser, Debug)]
 #[command(name = env!("CARGO_BIN_NAME"))]
@@ -29,30 +26,18 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     dbg!(&args);
 
-    let self_session_id = GnuId::new();
-    let bind = TcpListener::bind(format!("0.0.0.0:{}", args.bind)).await?;
-    let _handle = tokio::spawn(server(self_session_id, bind));
+    let _self_session_id = GnuId::new();
+    // let bind = TcpListener::bind(format!("0.0.0.0:{}", args.bind)).await?;
+    // let _handle = tokio::spawn(server(self_session_id, bind));
 
-    let remote = args.ping_to.parse().expect("Ping先アドレスの分析に失敗しました");
-    let stream = tokio::net::TcpStream::connect(remote).await?;
+    // let remote: SocketAddr = args.ping_to.parse().expect("Ping先アドレスの分析に失敗しました");
+    // let stream = tokio::net::TcpStream::connect(remote).await?;
     // let local_addr = stream.local_addr().unwrap();
-    let handshake = OutgoingPcpHandshake::new(stream, remote, None);
-    let (oleh, ret) = handshake.ping(self_session_id, Some(args.bind), args.check_port).await?;
+    // let handshake = OutgoingPcpHandshake::new(stream, remote, None);
+    // let (oleh, ret) = handshake.ping(self_session_id, Some(args.bind), args.check_port).await?;
 
-    dbg!(&oleh);
-    dbg!(&ret);
+    // dbg!(&oleh);
+    // dbg!(&ret);
 
     Ok(())
-}
-
-async fn server(_self_session_id: GnuId, bind: TcpListener) -> anyhow::Result<()> {
-    loop {
-        let (stream, _remote) = bind.accept().await?;
-
-        let mut framed = Framed::new(stream, AtomCodec::new());
-        let atom = framed.next().await;
-        dbg!(atom);
-
-        // TODO: return Oleh
-    }
 }
